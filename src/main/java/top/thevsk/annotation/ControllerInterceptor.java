543a3176310ed908be74f9504071693c69c84526
mengr <zafkielcn@qq.com>
2018-06-19 16:23:08 +0800

    commit

---
+++ src/main/java/top/thevsk/annotation/ControllerInterceptor.java
@@ -0,0 +1,16 @@
+package top.thevsk.annotation;
+
+import top.thevsk.aop.BotControllerInterceptor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE})
+public @interface ControllerInterceptor {
+
+    Class<? extends BotControllerInterceptor>[] value();
+}
